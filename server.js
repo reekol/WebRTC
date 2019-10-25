3d54292b35f2632e7a8f4aea4f20df812d4cc6e0
reekol <cupuyc@gmail.com>
2019-10-25 11:05:10 +0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,104 @@
+const WebSocket = require('ws')
+const https = require('https')
+const fs = require('fs')
+const users = {}
+const sendTo = (ws, message) => {  ws.send(JSON.stringify(message)) }
+const server = https.createServer({
+  cert: fs.readFileSync('/var/www/html/seqr.link/crt/certificate.pem'),
+  key: fs.readFileSync('/var/www/html/seqr.link/crt/key.pem')
+})
+
+const wss = new WebSocket.Server({ server })
+
+
+wss.on('connection', function connection(ws) {
+  console.log('User connected')
+
+  ws.on('message', message => {
+    let data = null
+
+    try {
+      data = JSON.parse(message)
+    } catch (error) {
+      console.error('Invalid JSON', error)
+      data = {}
+    }
+
+    switch (data.type) {
+      case 'login':
+        console.log('User logged', data.username)
+        if (users[data.username]) {
+          sendTo(ws, { type: 'login', success: false })
+        } else {
+          users[data.username] = ws
+          ws.username = data.username
+          sendTo(ws, { type: 'login', success: true })
+        }
+        break
+      case 'offer':
+        console.log('Sending offer to: ', data.otherUsername)
+        if (users[data.otherUsername] != null) {
+          ws.otherUsername = data.otherUsername
+          sendTo(users[data.otherUsername], {
+            type: 'offer',
+            offer: data.offer,
+            username: ws.username
+          })
+        }
+        break
+      case 'answer':
+        console.log('Sending answer to: ', data.otherUsername)
+        if (users[data.otherUsername] != null) {
+          ws.otherUsername = data.otherUsername
+          sendTo(users[data.otherUsername], {
+            type: 'answer',
+            answer: data.answer
+          })
+        }
+        break
+      case 'candidate':
+        console.log('Sending candidate to:', data.otherUsername)
+        if (users[data.otherUsername] != null) {
+          sendTo(users[data.otherUsername], {
+            type: 'candidate',
+            candidate: data.candidate
+          })
+        }
+        break
+      case 'close':
+        console.log('Disconnecting from', data.otherUsername)
+        users[data.otherUsername].otherUsername = null
+
+        if (users[data.otherUsername] != null) {
+          sendTo(users[data.otherUsername], { type: 'close' })
+        }
+
+        break
+
+      default:
+        sendTo(ws, {
+          type: 'error',
+          message: 'Command not found: ' + data.type
+        })
+
+        break
+    }
+  })
+
+  ws.on('close', () => {
+    if (ws.username) {
+      delete users[ws.username]
+
+      if (ws.otherUsername) {
+        console.log('Disconnecting from ', ws.otherUsername)
+        users[ws.otherUsername].otherUsername = null
+
+        if (users[ws.otherUsername] != null) {
+          sendTo(users[ws.otherUsername], { type: 'close' })
+        }
+      }
+    }
+  })
+});
+
+server.listen(3001)
